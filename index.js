2e57c44452fe70e4a07c64cf2af7d8ac007467fc
Hakan Yildiz <hakanyildiz248@gmail.com>
2019-07-03 06:57:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
 const var1 = 31;
 const var2 = 2 ;
 
-var multiply = var1 * var2 ;
+let multiply = var1 * var2 ;
 
-var random = Math.floor(Math.random()*10);
+let random = Math.floor(Math.random()*10);
+
+let mod = 0;
+
+mod %= 4;
+
+let max = Math.max(1,2,3,4,5,6,7,8,9,10,20);
